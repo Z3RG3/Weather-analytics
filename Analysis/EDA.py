5f0e27b14f9d2efddc7579e9bcd5e3e2b90de994
Z3RG3 <110048668+Z3RG3@users.noreply.github.com>
2025-04-24 14:22:50 +0200

    Redundant columns

--- Analysis/EDA.py
+++ Analysis/EDA.py
@@ -18,12 +18,35 @@ for day in data["forecast"]["forecastday"]:
 
 dfx = pd.DataFrame(all_hours)
 
-# Convert date and time to datetime
-dfx["datetime"] = pd.to_datetime(dfx["date"] + " " + dfx["time"])
+# Convert date and time to datetime, convert obj to type
+dfx["datetime"] = pd.to_datetime(dfx["time"])
 dfx = dfx.set_index("datetime").sort_index()
+dfx["will_it_rain"] = dfx["will_it_rain"].astype(bool)
+dfx["is_day"] = dfx["is_day"].astype(bool)
+dfx["wind_dir"] = dfx["wind_dir"].astype(str)
 
 # Drop redundant columns
-dfx = dfx.drop(columns=["date", "time"])
+dfx = dfx.drop(
+    columns=[
+        "date",
+        "time",
+        "gust_mph",
+        "vis_miles",
+        "will_it_snow",
+        "dewpoint_f",
+        "heatindex_f",
+        "windchill_f",
+        "feelslike_f",
+        "snow_cm",
+        "precip_in",
+        "pressure_in",
+        "wind_mph",
+        "temp_f",
+        "time_epoch",
+    ]
+)
 
 # Display the DataFrame
 print(dfx.head())
+
+# * EDA
